503c6ae061c7a2d2837042b65fa8e3f6f29b25ff
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2025-03-19 10:21:10 +0100

    Split the addli method up into createli and addli

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
 
-
-
-function addLi(item, itemClass){
     let getul = document.getElementById("list")
     let ulCount = getul.childElementCount
 
+function createLi(item, itemClass){
     let newLi = document.createElement("li")
     let newText = document.createTextNode(item)
-    newLi.setAttribute("id", ulCount)
-    newLi.setAttribute("class", itemClass)
 
+    newLi.setAttribute("class", itemClass)
     newLi.append(newText)
-    getul.append(newLi)
+
+    return newLi
 }
 
-addLi("Ford Truck", "unhealthy")
-addLi("Juice", "healthy")
+function addLi(id, li){
+    let liToAdd = li
+    liToAdd.setAttribute("id", id)
+
+    getul.append(liToAdd)
+}
+
+let li1 =createLi("Ford Truck", "unhealthy")
+let li2 = createLi("Juice", "healthy")
+
+addLi(ulCount, li1)
+addLi(ulCount, li2)
