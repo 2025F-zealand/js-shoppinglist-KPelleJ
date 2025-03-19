2e7e1db339516f1e573f926f2a0c1c51438e131d
Kasper Pelle Jensen <kaj007@edu.zealand.dk>
2025-03-19 10:13:23 +0100

    Added function to add list item

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+
+
+
+function addLi(item, itemClass){
+    let getul = document.getElementById("list")
+    let ulCount = getul.childElementCount
+
+    let newLi = document.createElement("li")
+    let newText = document.createTextNode(item)
+    newLi.setAttribute("id", ulCount)
+    newLi.setAttribute("class", itemClass)
+
+    newLi.append(newText)
+    getul.append(newLi)
+}
+
+addLi("Ford Truck", "unhealthy")
+addLi("Juice", "healthy")
